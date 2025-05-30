f2b01c10c71db429365a3e6e232ea758865afc45
Gurbachan27 <gurbachanparmarbinjon@gmail.com>
2025-05-30 17:16:29 -0400

    final commit

--- Lab02/lab2.js
+++ Lab02/lab2.js
@@ -4,31 +4,35 @@ const prompt = require('prompt');
 prompt.start();
 
 prompt.get(['userSelection'], function(err, result) {
-    const userSelection = result.userSelection.toUpperCase();
+    if (err) {
+        console.error('Error reading input.');
+        return;
+    }
 
-    const random = Math.random();
+    const userSelection = result.userSelection.toUpperCase();
+    let computerRandom = Math.random();
     let computerSelection = '';
 
-    if (random <= 0.34) {
+    if (computerRandom <= 0.34) {
         computerSelection = 'PAPER';
-    } else if (random <= 0.67) {
+    } else if (computerRandom <= 0.67) {
         computerSelection = 'SCISSORS';
     } else {
         computerSelection = 'ROCK';
     }
 
-    console.log('User selected:', userSelection);
-    console.log('Computer selected:', computerSelection);
+    console.log('User chose:', userSelection);
+    console.log('Computer chose:', computerSelection);
 
     if (userSelection === computerSelection) {
-        console.log("It's a tie!");
+        console.log("It's a tie");
     } else if (
         (userSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
         (userSelection === 'PAPER' && computerSelection === 'ROCK') ||
         (userSelection === 'SCISSORS' && computerSelection === 'PAPER')
     ) {
-        console.log('User Wins!');
+        console.log('User Wins');
     } else {
-        console.log('Computer Wins!');
+        console.log('Computer Wins');
     }
 });
