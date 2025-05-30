b59270698a8e0b86d423d2e0125202be675715b9
Gurbachan27 <gurbachanparmarbinjon@gmail.com>
2025-05-30 17:11:15 -0400

    updated code to have a random number

--- Lab02/lab2.js
+++ Lab02/lab2.js
@@ -7,3 +7,24 @@ prompt.start();
 prompt.get(['userSelection'], function(err, result) {
     console.log('User selected:', result.userSelection);
 });
+const prompt = require('prompt');
+
+prompt.start();
+
+prompt.get(['userSelection'], function(err, result) {
+    const userSelection = result.userSelection;
+    console.log('User selected:', userSelection);
+
+    const random = Math.random();
+    let computerSelection = '';
+
+    if (random <= 0.34) {
+        computerSelection = 'PAPER';
+    } else if (random <= 0.67) {
+        computerSelection = 'SCISSORS';
+    } else {
+        computerSelection = 'ROCK';
+    }
+
+    console.log('Computer selected:', computerSelection);
+});
