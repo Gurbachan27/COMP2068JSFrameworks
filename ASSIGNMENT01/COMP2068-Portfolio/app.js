2805f6ea5b9c342895a4dd2d000e410345020b16
Gurbachan27 <gurbachanparmarbinjon@gmail.com>
2025-06-13 17:57:09 -0400

    added basic code to each file

--- ASSIGNMENT01/COMP2068-Portfolio/app.js
+++ ASSIGNMENT01/COMP2068-Portfolio/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -18,7 +19,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// only one router
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
@@ -28,15 +28,13 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
     res.status(err.status || 500);
     res.render('error');
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
 });
 
 module.exports = app;
--- ASSIGNMENT01/COMP2068-Portfolio/routes/index.js
+++ ASSIGNMENT01/COMP2068-Portfolio/routes/index.js
@@ -1,23 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page */
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
     res.render('home', { title: 'Home' });
 });
 
-/* GET about page */
-router.get('/about', function(req, res) {
+router.get('/about', (req, res) => {
     res.render('about', { title: 'About Me' });
 });
 
-/* GET projects page */
-router.get('/projects', function(req, res) {
+router.get('/projects', (req, res) => {
     res.render('projects', { title: 'Projects' });
 });
 
-/* GET contact page */
-router.get('/contact', function(req, res) {
+router.get('/contact', (req, res) => {
     res.render('contact', { title: 'Contact Me' });
 });
 
